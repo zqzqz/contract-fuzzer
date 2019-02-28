d8930b5dfb73fc392da31f926447d4b27ddd5f2e
zqzqz <443291890@qq.com>
2019-02-28 19:29:21 +0800

    Update tests of slither

---
+++ src/python/test/test_slither.py
@@ -0,0 +1,17 @@
+from slither.slither import Slither
+from slither.printers.summary.slithir import PrinterSlithIR
+import os
+import logging
+
+logger = logging.getLogger("Slither")
+
+filename = os.path.join(os.getcwd(), '../../static/Test.sol')
+
+slither = Slither(filename)
+printer = PrinterSlithIR(slither, logger)
+contracts = slither._contracts_by_id.values()
+
+for contract in contracts:
+    printer.output(contract)
+    # see https://github.com/trailofbits/slither/blob/master/slither/printers/summary/slithir.py
+    # get contracts, functions, nodes, expressions from contract object
