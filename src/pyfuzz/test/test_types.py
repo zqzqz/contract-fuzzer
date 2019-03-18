dcea9b87d583e8b9f39b359b2d81c1b3bcaf3382
Xrosheart <31621210+YZ-WANG@users.noreply.github.com>
2019-03-18 09:44:54 +0800

    Update test_types.py

--- src/pyfuzz/test/test_types.py
+++ src/pyfuzz/test/test_types.py
@@ -2,9 +2,12 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from pyfuzz.evm_types.types import TypeHandler
 
-if __name__ == '__main__':
+def test():
     typeHandler = TypeHandler()
     print(typeHandler.generateValueByType("uint256", "min"))
     print(typeHandler.generateValueByType("uint256", "max"))
     print(typeHandler.generateValueByType("uint256", "random"))
     print(typeHandler.generateValueByType("uint256", "seed"))
+  
+if __name__ == '__main__':
+    test()
