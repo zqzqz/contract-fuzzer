2e94ae6b82e7711083337281f6bdb923955019de
Xrosheart <31621210+YZ-WANG@users.noreply.github.com>
2019-09-30 03:07:02 +0800

    add parse_trace and cover_metrics <TODO more>

--- src/pyfuzz/fuzzer/trace.py
+++ src/pyfuzz/fuzzer/trace.py
@@ -18,10 +18,12 @@ class TraceAnalyzer:
         return
           report (list(string)): found vulnerabilities
     """
-    def run(self, ptraces, ctraces):
-        p_pcs = self.get_pcs(ptraces)
+    def run(self, ptraces, pstate, ctraces, cstate, cfgAnalyzer):
+        p_paths = self.parse_trace(ptraces, pstate, cfgAnalyzer.opCFG)
+        c_paths = self.parse_trace(ctraces, cstate, cfgAnalyzer.opCFG)
         c_pcs = self.get_pcs(ctraces)
-        reward = self.path_variaty(p_pcs, c_pcs)
+        self.cfgAnalyzer = cfgAnalyzer
+        reward = self.cover_metrics(p_paths, c_paths)
         report = list(set(self.detector.run(ctraces)))
         return reward, report, c_pcs
 
@@ -53,8 +55,108 @@ class TraceAnalyzer:
                     pcs.append(c["pc"])
         return set(pcs)
 
-    def path_variaty(self, p_pcs, c_pcs):
-        if len(c_pcs - p_pcs) > 0:
-            return 1
+    def parse_trace(self, ctraces, cstate, opCFG):
+        path = {}
+        bbs = [block.start_offset for block in opCFG.basicblocks]
+        # At the first step of fuzzing, no tx in list
+        if cstate==None:
+            return None
+        '''
+        Format of path:
+        {funcName:{"trace":[bbs],"SSTOREs":{bb_1:{'SSTORE':2,},...},"CALL":{}}
+        '''
+        # TODO: whether the trace need to be REVERT when have any calls
+        # len(ctraces) = 3 + number of tx with calls; len(txlist) = 3
+        for i in range(3):
+            funcName = cstate.txList[i].name
+            path[funcName] = {}
+            path[funcName]["trace"] = []
+            path[funcName]["SSTOREs"] = {}
+            path[funcName]["CALLs"] = {}
+            cbb = 0
+            for state in ctraces[i]:
+                if state['pc'] in bbs:
+                    cbb = state['pc']
+                    path[funcName]["trace"].append(cbb)
+                if state['op'] in ["SSTORE"]: # Can add other opcodes
+                    if not(cbb in path[funcName]["SSTOREs"]):
+                        path[funcName]["SSTOREs"][cbb] = {}
+                        path[funcName]["SSTOREs"][cbb][state['op']] = 1
+                    elif not(state['op'] in path[funcName]["SSTOREs"][cbb]):
+                        path[funcName]["SSTOREs"][cbb][state['op']] = 1
+                    else:
+                        path[funcName]["SSTOREs"][cbb][state['op']] += 1
+                    # Also can count directly in cfg
+                if state["op"] in call_op:
+                    if not(cbb in path[funcName]["CALLs"]):
+                        path[funcName]["CALLs"][cbb] = {}
+                        path[funcName]["CALLs"][cbb][state['op']] = 1
+                    elif not(state['op'] in path[funcName]["CALLs"][cbb]):
+                        path[funcName]["CALLs"][cbb][state['op']] = 1
+                    else:
+                        path[funcName]["CALLs"][cbb][state['op']] += 1
+        return path
+
+    def cover_metrics(self,p_paths, c_paths):
+        if p_paths == None: return None
+        p_funcs = [funcName for funcName in p_paths]
+        c_funcs = []
+        reward = {}
+        '''
+        format of reward: {funcName:score,...}
+        '''
+        for funcName in c_paths:
+            c_funcs.append(funcName)
+            reward[funcName] = 0
+        if not(p_funcs == c_funcs):
+            # calculate reward
+            for i in range(len(c_funcs)-1, -1, -1):
+                if i == len(c_funcs) - 1: # Tx3: we hope it'trace has calls
+                    if p_funcs[i] == c_funcs[i]:
+                        if p_paths[c_funcs[i]]["trace"] == c_paths[c_funcs[i]]["trace"]:
+                            reward[c_funcs] -= 1
+                        else:
+                            count = 1
+                            for bbs in c_paths[c_funcs[i]]["CALLs"]:
+                                for call in c_paths[c_funcs[i]]["CALLs"][bbs]:
+                                    count += c_paths[c_funcs[i]]["CALLs"][bbs][call]
+                            for bbs in p_paths[p_funcs[i]]["CALLs"]:
+                                for call in p_paths[p_funcs[i]]["CALLs"][bbs]:
+                                    count -= p_paths[p_funcs[i]]["CALLs"][bbs][call]
+                            if count < 1: count = 1
+                            reward[c_funcs[i]] += count
+                    else:
+                        count = 0
+                        for bbs in c_paths[c_funcs[i]]["CALLs"]:
+                            for call in c_paths[c_funcs[i]]["CALLs"][bbs]:
+                                count += c_paths[c_funcs[i]]["CALLs"][bbs][call]
+                        if count: reward[c_funcs[i]] += 1
+                else:pass #TODO
         else:
-            return 0
+            for i in range(len(c_funcs)-1, -1, -1):
+                if p_paths[c_funcs[i]]["trace"] == c_paths[c_funcs[i]]["trace"]:
+                    if i == len(c_funcs) - 1 :
+                        reward[c_funcs] -= 1
+                    else:
+                        pass #TODO
+                else:
+                    count = 1 # means new blocks finded
+                    if i == len(c_funcs) - 1:
+                        for bbs in c_paths[c_funcs[i]]["CALLs"]:
+                            for call in c_paths[c_funcs[i]]["CALLs"][bbs]:
+                                count += c_paths[c_funcs[i]]["CALLs"][bbs][call]
+                        for bbs in p_paths[p_funcs[i]]["CALLs"]:
+                            for call in p_paths[p_funcs[i]]["CALLs"][bbs]:
+                                count -= p_paths[p_funcs[i]]["CALLs"][bbs][call]
+                        if count < 1: count = 1
+                    else:
+                        for bbs in c_paths[c_funcs[i]]["SSTOREs"]:
+                            for call in c_paths[c_funcs[i]]["SSTOREs"][bbs]:
+                                count += c_paths[c_funcs[i]]["SSTOREs"][bbs][call]
+                        for bbs in p_paths[p_funcs[i]]["SSTOREs"]:
+                            for call in p_paths[p_funcs[i]]["SSTOREs"][bbs]:
+                                count -= p_paths[p_funcs[i]]["SSTOREs"][bbs][call]
+                        if count < 1: count = 1
+                    reward[c_funcs[i]] += count
+        return reward
+
